368470f55c58b35575dd7bd8205fd60a084406df
joaovic499 <joaovictorsilva60@live.com>
2024-04-30 14:07:31 -0300

    Adição da rotas e estilização da Tela de criar

--- src/app/fruit/fruit-routing.module.ts
+++ src/app/fruit/fruit-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'fruit/home', component:HomeComponent},
+  {path: 'fruit', redirectTo:'fruit/home', pathMatch: 'full'},
+  {path: '', redirectTo:'fruit/home', pathMatch: 'full'},
+  {path: 'fruit/create', component: CreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
